e01c2a79dbd6cd4bada9dadb76b63bcb01d76af2
Hrobjartur Thorsteinsson <thorsteinssonh@gmail.com>
2016-05-18 23:02:02 +0800

    improved styling/links front page
    improved styling/links front page

--- web2py/applications/devicedb/controllers/default.py
+++ web2py/applications/devicedb/controllers/default.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from language_session import LanguageSession
+from datetime import datetime, timedelta
 
 @LanguageSession
 def index():
     if auth.has_membership('observer'):
         latest_history = db(db.device_history).select(orderby=~db.device_history.time_used,
                                                       limitby=(0,10))
-    else:
-        latest_history = None
+    # introduction
     title = T('CHC Healthcare Group')
     subtitle = T('medical device database')
     introductory_text = T('The CHCDeviceDB collects information about medical devices provisioned by the CHC Healthcare Group in Taiwan. If you need access to the data here within you must register and ask an administrator for privileges to access the data.')
-    message = T("hello")
+    # service summary
+    service_summary={}
+    service_summary['total_sites'] = db(db.site).count()
+    service_summary['total_devices'] = db(db.device).count()
+    service_summary['total_treatments'] = db(db.device_history.time_used < request.now).count()
+    service_summary['total_patients'] = len(db(db.device_history.time_used < request.now).select(db.device_history.site, db.device_history.patient_id, distinct=True))
     return locals()
 
 @LanguageSession
